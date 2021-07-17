8daf2a11f36d395c5a470d5d559b6c9b7b9620aa
Franco <52047668+FDelocca2305@users.noreply.github.com>
2021-07-17 12:53:03 -0300

    SpawnRandom entre limites, Movimiento maximo de la nave

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ShipController : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+    private Vector2 movement;
+
+    [Header("Speed")]
+    public float speed = 2f;
+
+    [Header("Limite Speed")]
+    public float limitSpeed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        movement = new Vector2(moveHorizontal, 0f);
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(movement * speed * 5f);
+
+        if (rigidbody.velocity.x >= limitSpeed)
+        {
+            rigidbody.velocity = new Vector2(limitSpeed, 0f);
+        }
+        else if (rigidbody.velocity.x <= (limitSpeed * -1))
+        {
+            rigidbody.velocity = new Vector2((limitSpeed * -1), 0f);
+        }
+
+    }
+}
---
+++ Assets/Scripts/SpawnerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerController : MonoBehaviour
+{
+    [Header("LimiteUno")]
+    [SerializeField]
+    private GameObject limitOne;
+
+    [Header("LimiteDos")]
+    [SerializeField]
+    private GameObject limitTwo;
+
+    [Header("Objeto a spawnear")]
+    public GameObject objectToSpawn;
+
+
+    private Vector2 position;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        var positionOne = limitOne.transform.position.x;
+        var positionTwo = limitTwo.transform.position.x;
+
+        position = new Vector2(Random.Range(positionOne >= 0 ? positionOne - 1f: positionOne + 1f, positionTwo >= 0 ? positionTwo - 1f : positionTwo + 1f), objectToSpawn.transform.position.y);
+
+        objectToSpawn.transform.position = position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
